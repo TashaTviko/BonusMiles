a281685c7238c806d1f353e7597da4d1eab5df20
Natalia Korobchenko <tasha.tviko@gmail.com>
2024-08-16 19:35:55 +0300

    add miles calculation 1

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,12 +1,10 @@
 public class BonusMilesService {
 
-    public int price (int a) {
+    int price;
+    int bonus = 20;
+
+    public int calculate(int price) {
         int miles = price / bonus;
         return miles;
     }
 }
-//int price = 9000; // стоимость билета
-//        int bonus = 20; // количество рублей для одной бонусной мили
-//        int miles = price / bonus;
-//        System.out.println("Количество бонусных миль:");
-//        System.out.println(miles);
